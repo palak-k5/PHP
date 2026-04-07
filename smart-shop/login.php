561efe384d818982c727a0f41c858d3dd39e0189
palak-k5 <palakkumrawat05@gmail.com>
2026-04-08 03:26:48 +0530

    login logic flow fixed

--- smart-shop/login.php
+++ smart-shop/login.php
@@ -1,43 +1,67 @@
-    <?php
-    // require "db.php";
+<?php
+    require "db.php";
     session_start();
- //if already logged in we need to check that so that user directly redirects to index.php;
-    // if(isset($_SESSION['username']))
-    //     {
-    //         header("Location: index.php");
-    //         exit();
-    //     }
 
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-    if(isset($_POST['username']))
+    //if already logged in we need to check that so that user directly redirects to index.php;
+    if(isset($_SESSION['user']))
+    {
+        header("Location: index.php");
+        exit();
+    }
+    else
+    {
+        if($_SERVER['REQUEST_METHOD']==='POST')
         {
-    //         // echo $_POST['username'];
-    //         // echo $_POST['password'];
-    //         $sql="select count(*) from users where name=$_POST['username']";
-    //         $result=$conn->query($sql);
-    //         if($result && $result->num_rows>0)
-    //             {
-                    $_SESSION['user']=$_POST['username'];
-                    $_SESSION['password']=$_POST['password'];
-                    // $_SESSION['email']=$_POST['email'];
-                    header("Location: index.php");
-                    exit();
-        //         }
-            
-        }
+            // echo $_POST['username'];
+            // echo $_POST['password'];
+            $user=$_POST['username']??"";
+            $password=$_POST['password']??"";
 
+            if(empty($user) || empty($password))
+            {
+                echo "Please fill all fields";
+            }
+            else
+            {
+                $sql="select * from users where name='$user'"; 
+                $result=($conn->query($sql))->fetch_assoc();
+                if($result)
+                {
+                    if(password_verify($password, $result['password']))
+                    {
+                        $_SESSION['user'] = $user;
+                        header("Location: index.php");
+                        exit();
+                    }
+                    else
+                    {
+                        echo "enter correct password";
+                    }
+                }
+                else
+                {
+                    echo "Add user to database , user does not exists";
+                }
+                
+            }
+        }
+    }
     //login persistence(protect routes as well)
-    //how will you aaccess the login session once logged in it should not go to the same login page
+    //how will you aaccess the login session once logged in it should not go to the same login page.  done
 
     //session storaage and local storage
     //get data frrom session itself
-    ?>
+?>
+<form method="POST">
+    Enter username:
+        <input type="text" name="username"><br><br>
+    Enter password:
+        <input type="password" name="password"><br>
+    <button type="submit">Login</button>
+</form>
 
-    <form method="POST">
-        Enter username:
-        <input type="text" name="username">
-        <input type="text" name="password">
-        <!-- <input type="email" name="email"> -->
 
-        <button type="submit">Login</button>
-    </form> 
+
