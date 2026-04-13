57c9dbc3e0687c17f19a9e0b0aed02c1dd94ee5e
palak-k5 <palakkumrawat05@gmail.com>
2026-04-14 04:48:50 +0530

    banking oops task (phase 2

---
+++ php_oops/banking.php
@@ -0,0 +1,153 @@
+
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+class Customer
+{
+    private static $id=100;
+    public $custid;
+    public $firstName;
+    public $lastName;
+    public $email;
+
+
+    public function __construct($fname,$lname,$email)
+    {
+        $this->custid=self::$id++;
+        $this->firstName=$fname;
+        $this->lastName=$lname;
+        $this->email=$email;
+        // $this->$custid means “variable variable” wrong
+
+    }
+    public function getcustdetails()
+    {
+        echo "Customer First name : "." ".$this->firstName."<br>";
+        echo "Customer last name : "." ".$this->lastName."<br>";
+        echo "Customer email : "." ".$this->email."<br>";
+
+
+    }
+    public function __destruct()
+    {
+        echo "destructor function called for Customer class object<br>";
+
+    }
+
+}
+
+class Account {
+    private static $acc_count=10000;
+    protected $accNo;
+    private $balance;
+
+    public function __construct($balance)
+    {
+        if($balance<0)
+            {
+                die("Balance cannot be negative");
+            }
+        $this->accNo=self::$acc_count++;
+        $this->balance=$balance;
+    }
+    public function getbalance()
+    {
+        return $this->balance;
+    }
+    public function getaccdetails()
+    {
+        echo "User with "." ". $this->accNo." as account number has balance of ".$this->getbalance()."<br>";
+    }
+    public function deposit($amt)
+    {
+        if($amt<=0)
+            {
+                die("enter correct amount");
+            }
+        $this->balance+=$amt;
+    }
+    public function withdraw($amt)
+    {
+        if ($amt <= 0) {
+                die("Withdraw amount should be valid");
+            }
+        if($amt>$this->balance)
+            {
+                die("you dont have enough balance");
+            }
+            
+        $this->balance-=$amt;
+    }
+    public function __destruct()
+    {
+        echo "destructor function called for account class object<br>";
+
+    }
+
+}
+
+class Transaction
+{
+    protected $amount;
+    public $type;
+    protected $date;
+
+    public function __construct($amount, $type)
+    {
+        if ($amount <= 0) {
+            die("Amount should be > 0");
+        }
+
+        $allowtrans= ["deposit", "withdraw"];
+
+        if (!in_array($type, $allowtrans)) {
+            die("Invalid transaction type");
+        }
+
+        $this->amount = $amount;
+        $this->type = $type;
+        $this->date = date("Y-m-d H:i:s");
+    }
+    public function gettransdetails()
+    {
+        echo "type: " . $this->type . "<br>";
+        echo "amount: " . $this->amount . "<br>";
+        echo "date: " . $this->date . "<br>";
+    }
+   
+
+    public function __destruct()
+    {
+        echo "destructor function called for transaction class object<br>";
+    }
+}
+
+
+$obj1= new Customer("Palak","Kumrawat","palak@gmail.com");
+$obj1->getcustdetails();
+
+// unset($obj1);  //destructor will be called
+// $acc1= new Account(-15000);
+
+$acc1= new Account(15000);
+
+
+// $acc2=new Account();
+// $acc2->getaccdetails();
+
+// echo $acc1->balance; 
+
+$acc1->getaccdetails();
+$acc1->deposit(20000);
+
+$acc1->getaccdetails();
+$acc1->withdraw(290);
+$acc1->getaccdetails();
+
+
+$trans1=new Transaction(200,"deposit");
+$trans1->gettransdetails();
+
+?>
